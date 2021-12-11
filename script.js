c795be0b067a743ad8ce176b44276a5cba6eadcf
Coder Suresh <48799677+coderSuresh@users.noreply.github.com>
2021-12-11 18:17:03 +0545

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var btn = document.querySelector("button")
 var input = document.querySelector("input")
 
-var mailFormat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+var mailFormat = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 btn.addEventListener("click", () => {
-    if (!input.value.match(mailFormat))
-        input.classList.add("active")
-    else
+    if (input.value.match(mailFormat) && input.value != "")
         input.classList.remove("active")
+    else
+        input.classList.add("active")
 })
